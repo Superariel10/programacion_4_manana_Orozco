282f6c7c9bbec408cb7c65900d392ae0cff062e5
Superariel10 <ariorozco2006@gmail.com>
2026-04-11 19:56:33 -0500

    09_if_simple.kt

--- modulo_1/basics/09_if_simple_mp.kt
+++ modulo_1/basics/09_if_simple_mp.kt
@@ -2,11 +2,11 @@ fun main() {
   println("Controles de Flujo")
   println("Condicional IF")
   println("Incluir Edad para el sacramiento: ")
-  val edad = readLine()?.toDoubleOrNull()?: 36
-  if(edad>=18){
-      println("Mayor de edad")
+  val edad = readLine()?.toDoubleOrNull()?: 36.0
+  if(edad>=18.0){
+    println("Mayor de edad")
   } 
-  if(edad<=17){
+  if(edad>=17.0){
     println("Menor de edad")
   }
   println("Edad registrada: $edad") 
