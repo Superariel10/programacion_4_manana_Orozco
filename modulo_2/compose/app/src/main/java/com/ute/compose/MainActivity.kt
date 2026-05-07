5707cef1de297e052b2455f8ef5a7935b60740a8
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-05-07 11:00:45 -0500

    Clase: 07-05-2026

--- modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
+++ modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.ute.compose.ui.material3.Paso01Compra
 import com.ute.compose.ui.material3.Paso01Suma
+import com.ute.compose.ui.material3.Paso02CardScreen
+import com.ute.compose.ui.material3.Paso03LazyColumnScreen
+import com.ute.compose.ui.material3.Paso04ScaffoldScreen
+import com.ute.compose.ui.material3.Paso05NavBarScreen
+import com.ute.compose.ui.material3.Paso06DialogosScreen
 import com.ute.compose.ui.screens.*
 import com.ute.material3.ui.screens.Paso01TextFieldScreen
 
@@ -27,11 +33,12 @@ class MainActivity : ComponentActivity() {
                 // ◀ Componentes Material 3:
                 //Paso01TextFieldScreen()
                 //Paso01Suma()
-                // Paso02_CardScreen()
-                // Paso03_LazyColumnScreen()
-                // Paso04_ScaffoldScreen()
-                // Paso05_NavBarScreen()
-                //Paso06_DialogosScreen()   // ← paso activo
+                //Paso01Compra()
+                //Paso02CardScreen()
+                //Paso03LazyColumnScreen()
+                //Paso04ScaffoldScreen()
+                //Paso05NavBarScreen()
+                Paso06DialogosScreen()   // ← paso activo
             }
         }
     }
