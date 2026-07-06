159a487566c279446f9f83e3ca7249d6081695b7
Superariel10 <ariel.orozco@ute.edu.ec>
2026-07-06 10:39:55 -0500

    flutter_shop_app checkpoint 05-06-07-2026

--- modulo13/flutter_shop_app/lib/presentation/navigation/app_router.dart
+++ modulo13/flutter_shop_app/lib/presentation/navigation/app_router.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shop_app/presentation/screens/cart/cart_screen.dart';
+import 'package:flutter_shop_app/presentation/screens/catalog/productdetailscreen.dart';
 import 'package:go_router/go_router.dart';
 import '../../domain/model/auth_state.dart';
 import '../providers/auth_provider.dart';
@@ -69,8 +71,15 @@ final routerProvider = Provider<GoRouter>((ref) {
           GoRoute(path: '/',        builder: (_, __) => const HomeScreen()),
           GoRoute(path: '/catalog', builder: (_, __) => const CatalogScreen()),
           GoRoute(
-            path:    '/product/:id',
-            builder: (_, s) => _PlaceholderScreen('Detalle #${s.pathParameters['id']} — M5'),
+            path: '/cart',
+            builder: (_, __) => const CartScreen(),
+          ),
+          GoRoute(
+            path: '/product/:id', // /catalog/1 → id=1
+            builder: (_, state) {
+              final id = int.parse(state.pathParameters['id']!);
+              return ProductDetailScreen(productId: id);
+            },
           ),
           GoRoute(path: '/cart',    builder: (_, __) => const _PlaceholderScreen('Carrito — M5')),
           GoRoute(path: '/orders',  builder: (_, __) => const _PlaceholderScreen('Mis pedidos — M6')),
